a99270fc039a9743cf2d2ac2be7f48f0086050a5
CSCD13 <carlossantiagocruz11@gmail.com>
2026-02-20 17:56:50 -0600

    Set up targets with PBR materials and sound effects

---
+++ Assets/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+
+
+public class CustomTrackableEventHandler : DefaultObserverEventHandler {    
+
+    [SerializeField] private AudioSource audioSource;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        audioSource.Play();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        audioSource.Stop();
+    }
+}
