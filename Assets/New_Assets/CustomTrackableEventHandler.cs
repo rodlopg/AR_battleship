a2a0a6c03a1f618cb6de2b5b26694fd65e88b282
Rodrigo López <rodlopg@gmail.com>
2026-02-20 18:14:43 -0600

    Changes to the menu

---
+++ Assets/New_Assets/CustomTrackableEventHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+
+
+public class CustomTrackableEventHandler : DefaultObserverEventHandler
+{
+
+    public bool isVideoEnabled;
+    
+    public VideoPlayer videoPlayer;
+    public RawImage rawImage;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(PrepareVideo());
+    }
+
+
+    private IEnumerator PrepareVideo()
+    {
+        videoPlayer.Prepare();
+
+        while (!videoPlayer.isPrepared)
+        {
+            yield return new WaitForSeconds(.5f);
+        }
+
+        rawImage.texture = videoPlayer.texture;
+
+        isVideoEnabled = true;
+    }
+
+
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        if (isVideoEnabled) videoPlayer.Play();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        if (isVideoEnabled) videoPlayer.Pause();
+    }
+}
---
+++ Assets/New_Assets/Video.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Video : MonoBehaviour
+{
+    public bool isVideoEnabled;
+    [Space]
+    public VideoPlayer videoPlayer;
+    public RawImage rawImage;
+
+    private void Start()
+    {
+        StartCoroutine(PrepareVideo());
+    }
+
+    private IEnumerator PrepareVideo()
+    {
+        videoPlayer.Prepare();
+
+        while (!videoPlayer.isPrepared)
+        {
+            yield return new WaitForSeconds(.5f);
+        }
+
+        rawImage.texture = videoPlayer.texture;
+
+        isVideoEnabled = true;
+    }
+
+}
